69e161d2ab5f7281d2e396e94ddea5a6ba7b94fb
tony <tony@kali>
2025-05-14 18:50:56 +0300

    Add Header

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+function Header(){
+    return(
+        <header>
+            <h2>Meta Trader</h2>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact Us</li>
+            </ul>
+        </header>
+    )
+};
+export default Header;
